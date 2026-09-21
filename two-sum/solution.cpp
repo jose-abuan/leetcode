02616f8a3cf88bf60dd217a78bb93fae528846ca
Jose Abuan <abuanjosel@gmail.com>
2026-09-21 10:26:30 -0600

    Added solution and tests to romanToInt and isPalindrome

--- two-sum/solution.cpp
+++ two-sum/solution.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
@@ -7,7 +9,7 @@ public:
         vector<int> solution;
         for(unsigned int i = 0; i < nums.size() - 1;i++){
             for(unsigned int j = i + 1; j < nums.size(); j++){
-                if(nums.at(i) + nums.at(j)){
+                if(nums.at(i) + nums.at(j) == target){
                     solution = {i, j};  
                     return solution;
                 }
@@ -15,4 +17,59 @@ public:
         }
         return solution;
     }
+
+    bool isPalindrome(int x){
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+        
+        // Take how many 10s are in this number
+        long long divisor = 1;
+        while(x / divisor >= 10){
+            divisor *= 10;
+        }
+
+        // Loop through the numbers comparing the left and right digit
+        // When you hit the last digit(s), if it's on it's own return true, else keep compare
+        while(x > 0){
+            int left_digit = x / divisor;
+            int right_digit = x % 10;
+
+            if(left_digit != right_digit){
+                return false;
+            }
+            // strip the 2 digits from the number
+            x = (x % divisor) / 10;
+            
+            // shrink the divisor since we took 2 numbers away
+            divisor /= 100;
+        }
+        return true;
+    }
+    
+    int romanToInt(string s){
+        int sum = 0;
+        int prevValue = 0;
+        unordered_map<char, int> romanToIntMap = {
+            {'I',1}, {'V',5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        for(char c: s){
+            int currValue = romanToIntMap[c];
+            sum += (currValue > prevValue) ? (currValue - 2*prevValue) : currValue;           
+            prevValue = currValue;
+        }
+        return sum;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        string longest_common =  
+        for (string each : strs){
+
+        }
+        return "";   
+    }
+
 };
+
+
--- two-sum/test.cpp
+++ two-sum/test.cpp
@@ -1,8 +1,22 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
+#include <string>
 #include "solution.cpp"
 using namespace std;
+
+// We make the assumption based on leetcode that each one input would only have one solution
+
+// Longest Common Prefix
+TEST(Solution, LCPCaseOne){
+    Solution sol;
+    vector<string> inputs = {"flower", "flow", "flight"};
+    EXPECT_EQ(sol.longestCommonPrefix(inputs), "fl");
+}
+
+
+
+
 
 // TEST(SuiteName, TestName)
 TEST(Solution, Two_Elements){
@@ -12,9 +26,84 @@ TEST(Solution, Two_Elements){
     EXPECT_EQ(sol.twoSum(input, 5), expected);
 } 
 
-TEST(Solution, Two_Elements){
+TEST(Solution, Three_elements){
     Solution sol;
-    vector<int> input = {3,2};
+    vector<int> input = {3,2,6,4};
     vector<int> expected = {0,1};
-    EXPECT_EQ(sol.twoSum(input, 5), expecteds);
+    EXPECT_EQ(sol.twoSum(input, 5), expected);
 } 
+
+TEST(Solution, Leetcode_public_1){
+    Solution sol;
+    vector<int> input = {2,7,11,15};
+    vector<int> expected = {0,2};
+    EXPECT_EQ(sol.twoSum(input, 13), expected);
+}
+
+
+TEST(Solution, Leetcode_public_2){
+    Solution sol;
+    vector<int> input = {3,2,4};
+    vector<int> expected = {1,2};
+    EXPECT_EQ(sol.twoSum(input, 6), expected);
+}
+
+// Palindrome Tests
+TEST(Solution, Is121Palindrome){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(121), true);
+}
+
+TEST(Solution, IsNegativePalindrome){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(-121), false);
+}
+
+TEST(Solution, GeneralFalsePalindromeCase){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(109121283), false);
+}
+
+TEST(Solution, RepDigitPalindrome){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(333333333), true);
+}
+
+TEST(Solution, Is10Palindrome){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(10), false);
+}
+
+TEST(Solution, OddPalindrome){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(12321), true);
+}
+
+TEST(Solution, PalindromeLeetcodeFailingTestCase){
+    Solution sol;
+    EXPECT_EQ(sol.isPalindrome(1000021), false);
+}
+
+
+// Roman to int tests
+TEST(Solution, caseOne){
+    Solution sol;
+    string s = "III";
+    EXPECT_EQ(sol.romanToInt(s), 3);
+}
+
+TEST(Solution, caseTwo){
+    Solution sol;
+    string s = "LVIII";
+    EXPECT_EQ(sol.romanToInt(s), 58);
+}
+
+TEST(Solution, caseThree){
+    Solution sol;
+    string s = "MCMXCIV";
+    EXPECT_EQ(sol.romanToInt(s), 1994);
+    
+}
+
+
+
